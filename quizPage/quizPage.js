636c7a15efefa25f37c2106ffacabceebcc09011
Aygen Yucel <aygenyucel@outlook.com>
2022-10-17 16:11:56 +0300

    created quiz page template

---
+++ quizPage/quizPage.js
@@ -0,0 +1,45 @@
+const questions = [
+  {
+    prompt: "Which type of JavaScript language is ___",
+    optionA: "Object-Oriented",
+    optionB: "Object-Based",
+    optionC: "Assembly-language",
+    optionD: "High-level",
+    answer: "B",
+  },
+  {
+    prompt: 'The "function" and " var" are known as:',
+    optionA: "Keywords",
+    optionB: "Data types",
+    optionC: "Declaration statements",
+    optionD: "Prototypes",
+    answer: "C",
+  },
+  {
+    prompt:
+      "Which one of the following is the correct way for calling the JavaScript code?",
+    optionA: "Preprocessor",
+    optionB: "Triggering Event",
+    optionC: "RMI",
+    optionD: "Function/Method",
+    answer: "D",
+  },
+  {
+    prompt:
+      "In the JavaScript, which one of the following is not considered as an error:",
+    optionA: "Syntax error",
+    optionB: "Missing of semicolons",
+    optionC: "Division by zero",
+    optionD: "Missing of Bracket",
+    answer: "C",
+  },
+  {
+    prompt:
+      " In JavaScript, what will be used for calling the function definition expression:",
+    optionA: "Function prototype",
+    optionB: "Function literal",
+    optionC: "Function calling",
+    optionD: "Function declaration",
+    answer: "B",
+  },
+];
