de0bc960c107e738a9c1c50ad7dc93e10243ebbf
Aygen Yucel <aygenyucel@outlook.com>
2022-10-20 13:48:59 +0300

    added the percentage feautures

---
+++ resultPage/resultPage.js
@@ -0,0 +1,36 @@
+let score = 0;
+let maxScore = 10;
+let currentScore = 8;
+let correctAnswers = currentScore;
+let wrongAnswers = maxScore - correctAnswers;
+let percentageOfCorrect = (correctAnswers / maxScore) * 100;
+let percentageOfWrong = (wrongAnswers / maxScore) * 100;
+
+console.log(percentageOfCorrect);
+
+window.onload = function () {
+  changePercentageOfCorrect();
+  changePercentageOfWrong();
+  changeNumberOfCorrect();
+  changeNumberOfWrong();
+};
+
+const changePercentageOfCorrect = function () {
+  let correctPercentageElement = document.querySelector(".correct-percentage");
+  correctPercentageElement.innerHTML = percentageOfCorrect + "%";
+};
+
+const changePercentageOfWrong = function () {
+  let wrongPercentageElement = document.querySelector(".wrong-percentage");
+  wrongPercentageElement.innerHTML = percentageOfWrong + "%";
+};
+
+const changeNumberOfCorrect = function () {
+  let correctNumberElement = document.getElementById("correctNumber");
+  correctNumberElement.innerText = correctAnswers;
+};
+
+const changeNumberOfWrong = function () {
+  let wrongNumberElement = document.getElementById("wrongNumber");
+  wrongNumberElement.innerText = wrongAnswers;
+};
